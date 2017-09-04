3174e41966e3618e6608f8e553641487ad84f53d
Lynton Knapton <ljknapton@hotmail.co.uk>
2017-09-04 17:57:07 +0100

    terminal-apps v0.1 Lynton Knapton <ljknapton@hotmail.co.uk>Lynton Knapton 17cdf83 2 changed files Project_01_Termin…\arithmetic_test.py Project_01_Terminal_Apps\main.py

--- Project_01_Terminal_Apps/main.py
+++ Project_01_Terminal_Apps/main.py
@@ -1,17 +1,15 @@
-## Project_01_Terminal_Apps
-## Date: 31/08/2017    
-## By nurdy 
-
-#!/usr/bin/env python
-
 import time
 from arithmetic_test import *
 import os
 
-
+os.system("mode con cols=80 lines=25")
 
 print('welcome to nurdys tools')
 time.sleep(1)
 os.system('cls')
+
+##import os
+##hw = os.get_terminal_size()
+##print("Height:", hw[1], "Width:", hw[0])
 
 welcome()
